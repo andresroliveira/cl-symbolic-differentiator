0d7dc77afe50534281239d1f4335ef4b30f1143c
Andrês <andresroliveira98@gmail.com>
2026-01-16 17:11:05 -0300

    Add support for trig (sin/cos), exp/log and constant folding

--- diferenciador.lisp
+++ diferenciador.lisp
@@ -47,6 +47,32 @@
              (list '* n (list 'expt u (list '- n 1)))
              (deriv u var))))
 
+    ;; --- TRIGONOMETRIA E EXPONENCIAL ---
+
+    ;; Regra: sin(u) -> cos(u) * du
+    ((eq (first expr) 'sin)
+     (let ((u (second expr)))
+       (list '* (list 'cos u)
+             (deriv u var))))
+
+    ;; Regra: cos(u) -> -sin(u) * du
+    ((eq (first expr) 'cos)
+     (let ((u (second expr)))
+       (list '* (list '* -1 (list 'sin u)) ;; Representa -sin(u)
+             (deriv u var))))
+
+    ;; Regra: exp(u) -> exp(u) * du
+    ((eq (first expr) 'exp)
+     (let ((u (second expr)))
+       (list '* (list 'exp u)
+             (deriv u var))))
+
+    ;; Regra: log(u) -> (1/u) * du
+    ((eq (first expr) 'log)
+     (let ((u (second expr)))
+       (list '* (list '/ 1 u)
+             (deriv u var))))
+
     (t (error "Não sei derivar isso: ~a" expr))))
 
 (defun simplificar (expr)
@@ -66,6 +92,8 @@
             ((and (numberp u) (numberp v)) (+ u v))
             ((eq u 0) v)
             ((eq v 0) u)
+            ((and (numberp u) (listp v) (eq (first v) '+) (numberp (second v)))
+             (list '+ (+ u (second v)) (third v)))
             (t (list '+ u v))))
 
          ;; --- PRODUTO (*) ---
@@ -75,9 +103,11 @@
             ((or (eq u 0) (eq v 0)) 0)
             ((eq u 1) v)
             ((eq v 1) u)
+            ((and (numberp u) (listp v) (eq (first v) '*) (numberp (second v)))
+             (list '* (* u (second v)) (third v)))
             (t (list '* u v))))
 
-         ;; --- SUBTRAÇÃO (-) [NOVO!] ---
+         ;; --- SUBTRAÇÃO (-)  ---
          ((eq op '-)
           (cond
             ((and (numberp u) (numberp v)) (- u v)) ;; Resolve números: 3 - 2 = 1
@@ -102,6 +132,30 @@
             ((eq v 1) u)               ;; Algo elevado a 1 é ele mesmo
             ((and (numberp u) (numberp v)) (expt u v))
             (t (list 'expt u v))))
+
+         ;; --- TRIGONOMETRIA E EXPONENCIAL ---
+
+         ((eq op 'sin)
+          (cond ((eq u 0) 0)               ;; sin(0) = 0
+                ((numberp u) (sin u))      ;; Calcula se for número
+                (t (list 'sin u))))
+
+         ((eq op 'cos)
+          (cond ((eq u 0) 1)               ;; cos(0) = 1
+                ((numberp u) (cos u))      ;; Calcula se for número
+                (t (list 'cos u))))
+
+         ((eq op 'exp)
+          (cond ((eq u 0) 1)               ;; e^0 = 1
+                ((eq u 1) (exp 1))         ;; e^1 = número de Euler (aprox)
+                ((numberp u) (exp u))
+                (t (list 'exp u))))
+
+         ((eq op 'log)
+          (cond ((eq u 1) 0)               ;; ln(1) = 0
+                ((eq u (exp 1)) 1)         ;; ln(e) = 1 (simbólico, difícil de pegar exato float)
+                ((numberp u) (log u))
+                (t (list 'log u))))
 
          ;; Padrão
          (t (list op u v)))))))
